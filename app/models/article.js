f631eefdbc3fc106b29e2760d70e0ebe3eaeb31c
Paul Wells <pawell@microsoft.com>
2016-07-14 08:49:07 -0700

    testing articles

--- app/models/article.js
+++ app/models/article.js
@@ -1,12 +1,10 @@
 // Example model
 
-
 function Article (opts) {
   if(!opts) opts = {};
-  this.title = opts.title || '';
-  this.url = opts.url || '';
-  this.text = opts.text || '';
+  this.title = opts.title || 'Best Article';
+  this.url = opts.url || 'http://bestarticle.com';
+  this.text = opts.text || 'The best article text';
 }
 
 module.exports = Article;
-
