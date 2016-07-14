5480522767a7963c486c9715b5c20d76860cc107
Paul Wells <pawell@microsoft.com>
2016-07-14 11:04:02 -0700

    Adds simple fetching of news articles

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,15 +1,34 @@
 var express = require('express'),
   router = express.Router(),
   Article = require('../models/article');
+  
+var request = require('request-promise');
+var feed = require('feed-read');
+var bluebird = require('bluebird');
+
 
 module.exports = function (app) {
   app.use('/', router);
 };
 
 router.get('/', function (req, res, next) {
-  var articles = [new Article(), new Article()];
-    res.render('index', {
-      title: 'Generator-Express MVC',
-      articles: articles
+  var articles = fetchArticles()
+    .then(function (articles) {
+        res.render('index', {
+            title: 'News App',
+            articles: articles
+        });
     });
 });
+
+var fetchArticles = function () {
+    return new Promise(function (resolve, reject) {
+        feed('http://rss.msn.com/en-us/money?feedoutput=rss', function (err, articles) {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(articles);
+        });
+    });
+}
